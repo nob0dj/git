680c5600327ad9ee37f3a32f41d480c0947bb4be
shqkel <shqkel1863@gmail.com>
2018-05-29 17:34:08 +0900

    Team-SwitchTo [new branch]에서 branchTest입력후, checkout함.
    출력문 추가 : "I'm on branchTest!"

--- 01_HelloGit/src/com/kh/git/HelloGit.java
+++ 01_HelloGit/src/com/kh/git/HelloGit.java
@@ -3,8 +3,10 @@ package com.kh.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//파일을 생성하면 파일이 속한 트리구조 전체에 물음표가 생기며,
+		//not tracing(추적중 아님)을 의미함.
+		System.out.println("Hello Git!");
+		
 	}
 
 }
