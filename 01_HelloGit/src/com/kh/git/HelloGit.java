67d32e441215fa6e7164daaffb8bdf8707051d86
shqkel <shqkel1863@gmail.com>
2018-05-30 00:30:49 +0900

    another commit

--- 01_HelloGit/src/com/kh/git/HelloGit.java
+++ 01_HelloGit/src/com/kh/git/HelloGit.java
@@ -21,6 +21,7 @@ public class HelloGit {
 		System.out.println("branchTest : test메소드생성!!");
 		System.out.println(".gitignore not working!!");
 		System.out.println(".gitignore not working2!!");
+		System.out.println(".gitignore not working3!!");
 		
 	}
 
