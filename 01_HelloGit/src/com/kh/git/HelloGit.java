39d3ded61f8210231eeba3ddb6830219931cd07b
shqkel <shqkel1863@gmail.com>
2018-05-29 17:38:00 +0900

    Team-SwitchTo [new branch]에서 branchTest입력후, checkout함.
    출력문 추가 : "new branch : branchTest"

--- 01_HelloGit/src/com/kh/git/HelloGit.java
+++ 01_HelloGit/src/com/kh/git/HelloGit.java
@@ -7,6 +7,12 @@ public class HelloGit {
 		//not tracing(추적중 아님)을 의미함.
 		System.out.println("Hello Git!");
 		
+		//branchTest
+		//프로젝트 루트에서 우클릭, 
+		//Team-SwitchTo [new branch]에서 branchTest입력후, checkout함.
+		//checkout : 선택한 브랜치로 작업이동
+		System.out.println("I'm on branchTest!");
+		
 	}
 
 }
